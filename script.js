98e8e00ab814ecf668bb90610669b3a8f7b1cda8
Rex Flores <Rexgflores@gmail.com>
2020-12-09 21:27:07 -0800

    added console.logs

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function writePassword() {
 
 // Created a generate password function
 // TODO: Write all logic within this function
-// function generatePassword(){ 
+function generatePassword(){ 
     // bank of arrays for password characters
     var charCount = prompt("How many characters do you want your password to be?")
     var upperCaseConfirm =  confirm("Would you like to use capital letters?");
@@ -30,29 +30,36 @@ function writePassword() {
 // begin user input chain
     prompt("How many characters do you want your password to be?")
         if (charCount>=8 && charCount<=128)
+        console.log(charCount)
     confirm("Would you like to use uppercase letters?")
         if (upperCaseConfirm ===true) {
         finalArray = finalArray + upperCase
+        console.log(finalArray)
         }
     
     confirm("Would you like to use lowercase letters?")
         if (lowerCaseConfirm===true){
             finalArray += lowerCase
+            console.log(finalArray)
         }
     confirm("Would you like to use numbers?")
         if (numConfirm===true){
             finalArray += numbers
+            console.log(finalArray)
         }
     confirm("Would you like to use special characters?")
        if (spxCharConfirm===true){
            finalArray += spxChar
+           console.log(finalArray)
        }
     
     for (let i = 0; i <charCount; i++) {
            finalPass += finalArray[randomNum]
-            
+           console.log(finalArray)
         }
-// }
+    return (finalPass)
+}
+console.log(password)
 // Created confirms for uppercase, lowercase, numbers, and special characters.
 // Prompt user to ask how many chars they want (8-128)
 // Created conditional to make sure user has chosen at least one type of char
@@ -60,5 +67,6 @@ function writePassword() {
 // created an empty array to hold user choices
 // created if/else staements that checks the confirms by the user and based on response push those characters to empty array
 // Created "for" loop to loop over final Array. It should run based on user prompt (How many characters?)
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
