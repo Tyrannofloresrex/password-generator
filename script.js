785e8a8e3b6aa29fb5b0974fc2b68a3c78ed6296
Rex Flores <Rexgflores@gmail.com>
2020-12-09 20:06:38 -0800

    added if/then statements with array results

--- script.js
+++ script.js
@@ -9,11 +9,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
-var upperCase = ["A B C D E F G H I K L M N O P Q R S T V X Y Z"]
-var lowerCase = ["a b c d e f g h i j k l m n o p q r s t u v w x z"]
-var numbers = [0123456789]
-var spxChar = ["!#$%&()*+,-./:;<=>?@[\]^_{|}~"]
-var finalArray = ''
+
 // Created a generate password function
 // TODO: Write all logic within this function
 function generatePassword(){ 
@@ -21,14 +17,25 @@ function generatePassword(){
     var lowerCaseConfirm = confirm("Would you like to use lowercase letters?");
     var numConfirm = confirm ("Would you like to use numbers?")
     var spxCharConfirm = confirm ("Would you like to use special characters?")
-
-    confirm("Would you like to use capital letters?"){
-        if upperCaseConfirm ===true{
-
+    var upperCase = ["A B C D E F G H I K L M N O P Q R S T V X Y Z"]
+    var lowerCase = ["a b c d e f g h i j k l m n o p q r s t u v w x z"]
+    var numbers = [0123456789]
+    var spxChar = ["!#$%&()*+-./:;<=>?@[\]^_{|}~"]
+    var finalArray = ''
+    confirm("Would you like to use uppercase letters?")
+        if (upperCaseConfirm ===true) {
+        finalArray = finalArray + upperCase
         }
-        
-    }
-
+    
+    confirm("Would you like to use lowercase letters?")
+        if (lowerCaseConfirm===true){
+            finalArray = finalArray + lowerCase
+        }
+    confirm("Would you like to use numbers?")
+        if (numConfirm===true){
+            finalArray = finalArray + numbers
+        }
+    confirm ("Would you like to use special characters?")
 }
 // TODO: Create confirms for uppercase, lowercase, numbers, and special characters.
 // TODO: Prompt user to ask how many chars they want (8-128)
