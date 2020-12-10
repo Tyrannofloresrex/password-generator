3aec3e7c50515cdb0d75306b97b99cda6ca6c23c
Rex Flores <Rexgflores@gmail.com>
2020-12-09 21:14:08 -0800

    added for loop to generate final password, added var for character count random number

--- script.js
+++ script.js
@@ -12,16 +12,24 @@ function writePassword() {
 
 // Created a generate password function
 // TODO: Write all logic within this function
-function generatePassword(){ 
+// function generatePassword(){ 
+    // bank of arrays for password characters
+    var charCount = prompt("How many characters do you want your password to be?")
     var upperCaseConfirm =  confirm("Would you like to use capital letters?");
     var lowerCaseConfirm = confirm("Would you like to use lowercase letters?");
     var numConfirm = confirm ("Would you like to use numbers?")
     var spxCharConfirm = confirm ("Would you like to use special characters?")
     var upperCase = ["A B C D E F G H I K L M N O P Q R S T V X Y Z"]
     var lowerCase = ["a b c d e f g h i j k l m n o p q r s t u v w x z"]
-    var numbers = [0123456789]
-    var spxChar = ["!#$%&()*+-./:;<=>?@[\]^_{|}~"]
+    var numbers = [1234567890]
+    var spxChar = ["!#$%&?@^"]
+    // final accumulated array and final password with random number
     var finalArray = ''
+    var finalPass = ''
+    var randomNum = Math.floor(math.random()*charCount)
+// begin user input chain
+    prompt("How many characters do you want your password to be?")
+        if (charCount>=8 && charCount<=128)
     confirm("Would you like to use uppercase letters?")
         if (upperCaseConfirm ===true) {
         finalArray = finalArray + upperCase
@@ -29,20 +37,28 @@ function generatePassword(){
     
     confirm("Would you like to use lowercase letters?")
         if (lowerCaseConfirm===true){
-            finalArray = finalArray + lowerCase
+            finalArray += lowerCase
         }
     confirm("Would you like to use numbers?")
         if (numConfirm===true){
-            finalArray = finalArray + numbers
+            finalArray += numbers
         }
-    confirm ("Would you like to use special characters?")
-}
-// TODO: Create confirms for uppercase, lowercase, numbers, and special characters.
-// TODO: Prompt user to ask how many chars they want (8-128)
-// TODO: Create conditional to make sure user has chosen at least one type of char
-// TODO: create array of letters, numbers and special characters
-// TODO: create an empty array to hold user choices
-// TODO: create if/else staements that checks the confirms by the user and based on response push those characters to empty array
-// TODO: Create "for" loop to loop over final Array. It should run based on user prompt (How many characters?)
+    confirm("Would you like to use special characters?")
+       if (spxCharConfirm===true){
+           finalArray += spxChar
+       }
+    
+    for (let i = 0; i <charCount; i++) {
+           finalPass += finalArray[randomNum]
+            
+        }
+// }
+// Created confirms for uppercase, lowercase, numbers, and special characters.
+// Prompt user to ask how many chars they want (8-128)
+// Created conditional to make sure user has chosen at least one type of char
+// created array of letters, numbers and special characters
+// created an empty array to hold user choices
+// created if/else staements that checks the confirms by the user and based on response push those characters to empty array
+// Created "for" loop to loop over final Array. It should run based on user prompt (How many characters?)
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
